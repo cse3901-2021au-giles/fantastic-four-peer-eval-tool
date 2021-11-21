2e5b70330241f7130d6cf6826a03f76b91a9c020
OneMoreOneUp <jetportal@PugDuck.localdomain>
2021-11-21 18:49:12 -0500

    Added constraints to score and reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    belongs_to :peer_evaluation
+    belongs_to :user
+    has_many :scores
+    validates :comment, length: { maximum: 250 }
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,4 @@
 class Score < ApplicationRecord
+    validates :value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10,  only_integer: true }
+    belongs_to :review
 end
