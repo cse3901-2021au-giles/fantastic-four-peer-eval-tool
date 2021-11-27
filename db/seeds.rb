c5abc9a1cb443d4d008cd5b62bd069a516bc19c5
Matthew Anakin Crabtree <matthew@linoak.com>
2021-11-27 18:28:09 -0500

    Added database back to .gitignore and instead seed the example users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Example Users
+User.create(name: 'Student', email: 'student@3901.com', password: 'password', admin_level: 'Student')
+User.create(name: 'Teacher', email: 'teacher@3901.com', password: 'password', admin_level: 'Teacher')
+User.create(name: 'TA', email: 'ta@3901.com', password: 'password', admin_level: 'TA')
+User.create(name: 'System', email: 'system@3901.com', password: 'password', admin_level: 'System')
