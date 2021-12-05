4dbf1b1a3a436268bb04e85108a777bc44e611f4
OneMoreOneUp <jetportal@PugDuck.localdomain>
2021-12-04 22:20:08 -0500

    Added button to start peer evaluations and view scores for each team on the team index page.

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,8 @@
 module TeamsHelper
+    def evals_done? team
+        PeerEvaluation.where(team_id: team.id).collect.each do |eval|
+            return false unless eval.completed
+        end
+        true
+    end
 end
