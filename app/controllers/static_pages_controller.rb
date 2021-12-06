579eb647e39c48d178591fa3df1ba370b6079624
OneMoreOneUp <matthew@linoak.com>
2021-12-05 20:48:22 -0500

    Roster and My Teams page redirect you to home if you are not logged in.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  before_action :is_logged_in, only: [:roster, :my_teams]
+
   def home
   end
 
@@ -10,6 +12,15 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def my_teams
+  end
+
   def contact
   end
+
+  private
+    # Redirects to home if not logged in
+    def is_logged_in
+      redirect_to(root_url) unless logged_in?
+    end
 end
