5ca3d7d3fe9929f4f3d47b85e0a97c9c8561c97e
OneMoreOneUp <jetportal@PugDuck.localdomain>
2021-12-05 19:12:53 -0500

    Added 'My Teams' page to show teams that the current user is a part of.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :memberships
   root   'static_pages#home'
+  get    '/my_teams',    to: 'static_pages#my_teams'
   get    '/roster',    to: 'static_pages#roster'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
